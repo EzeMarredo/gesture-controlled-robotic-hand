6d50ad1f146423ed0d4ea203e17b8dc978bf8ed3
Ezequiel Marredo <ezemarredo@live.com>
2026-03-30 12:06:07 -0300

    Configurado giro servomotor para un dedo

--- ESP_servo_control/main/main.c
+++ ESP_servo_control/main/main.c
@@ -4,11 +4,38 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/uart.h"
+#include "driver/ledc.h"
 
 #define UART_PC     UART_NUM_0   // USB (ROS + monitor)
 #define UART_DEBUG  UART_NUM_2   // Pines físicos
 
 #define BUF_SIZE 1024
+
+int duty = 1638; // The duty will vary from 1638 to 3276
+
+void servo_init(void *arg)
+{
+    
+    ledc_timer_config_t timer_conf = {
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0,
+        .duty_resolution = LEDC_TIMER_15_BIT,
+        .freq_hz = 50, // 50 Hz for servo control
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+
+    ledc_timer_config(&timer_conf); // Configure LEDC timer
+
+    ledc_channel_config_t channel_conf = {
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .channel = LEDC_CHANNEL_0,
+        .timer_sel = LEDC_TIMER_0,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = 25, // GPIO pin connected to servo signal
+        .duty = duty
+    };
+    ledc_channel_config(&channel_conf); // Configure LEDC channel
+}
 
 void uart_init_all(void)
 {
@@ -36,8 +63,10 @@ void uart_init_all(void)
 void app_main(void)
 {
     uart_init_all();
+    servo_init(NULL);
 
     uint8_t data[BUF_SIZE];
+
 
     while (1)
     {
@@ -60,11 +89,16 @@ void app_main(void)
                     "Angles: %.2f %.2f %.2f %.2f %.2f\n",
                     angles[0], angles[1], angles[2],
                     angles[3], angles[4]);
+                
+                int rotate = 1638 + (int)(angles[1] * (3276 - 1638) / 180.0);
 
-                // 👉 Mostrar en monitor (USB)
+                ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, rotate);
+                ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+
+                // Mostrar en monitor (USB)
                 uart_write_bytes(UART_PC, msg, msg_len);
 
-                // 👉 (Opcional) duplicar a UART2
+                // duplicar a UART2
                 uart_write_bytes(UART_DEBUG, msg, msg_len);
             }
             else
